f09de8c1a66754f0a7601a4d6090e47ec1535228
Alexander Bantyev <alexander.bantyev@tweag.io>
2022-04-21 21:31:07 +0300

    Make the module better

--- nix/modules/mina.nix
+++ nix/modules/mina.nix
@@ -13,9 +13,17 @@ inputs:
           type = package;
           default = inputs.self.packages.${pkgs.system}.default;
         };
+        client-port = lib.mkOption {
+          type = port;
+          default = 8301;
+        };
         external-port = lib.mkOption {
           type = port;
           default = 8302;
+        };
+        rest-port = lib.mkOption {
+          type = port;
+          default = 3085;
         };
         external-ip = lib.mkOption {
           type = nullOr
@@ -24,6 +32,29 @@ inputs:
         };
         protocol-version = lib.mkOption {
           type = nullOr (strMatching "[0-9]+[.][0-9]+[.][0-9]+");
+          default = null;
+        };
+        log-level = lib.mkOption {
+          type = enum [
+            "Spam"
+            "Trace"
+            "Debug"
+            "Info"
+            "Warn"
+            "Error"
+            "Faulty_peer"
+            "Fatal"
+          ];
+          default = "Info";
+        };
+        disable-node-status = lib.mkOption {
+          type = bool;
+          default = false;
+        };
+
+        waitForRpc = lib.mkOption {
+          type = bool;
+          default = true;
         };
         extraArgs = lib.mkOption {
           type = listOf str;
@@ -35,22 +66,30 @@ inputs:
   config = let
     cfg = config.services.mina;
     config-file = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
+    inherit (lib) escapeShellArg optionalString optional;
+    optionalFlag = opt:
+      optionalString (!isNull cfg.${opt}) "--${opt} ${cfg.${opt}}";
   in {
     networking.firewall.allowedTCPPorts = [ cfg.external-port ];
     systemd.services.mina = lib.mkIf cfg.enable {
       wantedBy = [ "multi-user.target" ];
-      path = [ cfg.package ];
+      path = [ cfg.package ] ++ optional cfg.waitForRpc pkgs.netcat;
       script = ''
         mina daemon \
           --config-file ${config-file} \
           --config-dir "$STATE_DIRECTORY" \
           --working-dir "$STATE_DIRECTORY" \
+          --log-level ${cfg.log-level} \
           --external-port ${toString cfg.external-port} \
-          ${
-            lib.optionalString (!isNull cfg.external-ip)
-            "--external-ip ${cfg.external-ip}"
-          } \
-          ${toString (map lib.escapeShellArg cfg.extraArgs)}
+          --client-port ${toString cfg.client-port} \
+          --rest-port ${toString cfg.rest-port} \
+          ${optionalFlag "external-ip"} \
+          ${optionalFlag "protocol-version"} \
+          ${optionalString cfg.disable-node-status "--disable-node-status"}
+          ${toString (map escapeShellArg cfg.extraArgs)} \
+          &
+        ${optionalString cfg.waitForRpc
+        "until nc -z 127.0.0.1 ${cfg.client-port}; do sleep 1; done"}
       '';
       serviceConfig = {
         PrivateTmp = true;
@@ -59,6 +98,7 @@ inputs:
         User = "mina";
         Group = "mina";
         StateDirectory = "mina";
+        Type = "forking";
       };
     };
   };
